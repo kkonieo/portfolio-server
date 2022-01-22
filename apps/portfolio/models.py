c9e2e325d5b1e0b734ad7d374d1bf84e69dc1bff
hmkim199 <hmkim199@gmail.com>
2022-01-22 23:41:13 +0900

    feat: add portfolio model (#6)

--- apps/portfolio/models.py
+++ apps/portfolio/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from apps.core.models import TimeStampModel
+
+
+class Portfolio(TimeStampModel):
+    """
+    Portfolio 모델
+    """
+
+    title = models.CharField(verbose_name="포트폴리오 제목", max_length=40)
+    content = models.TextField(verbose_name="포트폴리오 내용")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name="사용자", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.title
