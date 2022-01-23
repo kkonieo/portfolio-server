cc823ce206b9fc1362d2f45c9315e25522f901af
hmkim199 <hmkim199@gmail.com>
2022-01-24 01:09:09 +0900

    feat: add liker column in project

--- apps/project/models.py
+++ apps/project/models.py
@@ -12,10 +12,18 @@ class Project(TimeStampModel):
     title = models.CharField(verbose_name="프로젝트 제목", max_length=40)
     content = models.TextField(verbose_name="프로젝트 내용")
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, verbose_name="사용자", on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL,
+        verbose_name="사용자",
+        on_delete=models.CASCADE,
+        related_name="project_user",
     )
     thumbnail = models.OneToOneField(
         Image, verbose_name="프로젝트 썸네일", on_delete=models.CASCADE
+    )
+    liker = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        verbose_name="프로젝트에 좋아요 남긴 사용자들",
+        related_name="project_liker",
     )
 
     def __str__(self):
