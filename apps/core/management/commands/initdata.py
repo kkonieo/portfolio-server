7c2b9d65bf74d7b596a9c5f3c0141c0c43b12e64
Yeonggwang <immutable000@gmail.com>
2022-01-27 02:32:29 +0900

    feat: add initdata Django command

---
+++ apps/core/management/commands/initdata.py
@@ -0,0 +1,24 @@
+import os
+from glob import glob
+
+from django.conf import settings
+from django.core import management
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "기본 기술스택 데이터를 추가합니다."
+
+    def handle(self, *args, **options):
+
+        # fixture 디렉터리 경로
+        fixture_directory = os.path.join(settings.BASE_DIR, "fixtures")
+
+        # fixtrue 디렉터리가 존재하는지 체크
+        if not os.path.isdir(fixture_directory):
+            raise FileNotFoundError(f"{fixture_directory} 디렉터리가 존재하지 않습니다.")
+
+        # json 파일만 가져온다
+        files = glob(f"{fixture_directory}/*.json")
+        for file in files:
+            management.call_command("loaddata", file)
