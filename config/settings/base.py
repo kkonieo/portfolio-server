2f5a2249027f55201214b3668d58552e40dd496e
2ykwang <csdp000@naver.com>
2022-01-26 14:54:05 +0900

    feat: implement user login

--- config/settings/base.py
+++ config/settings/base.py
@@ -7,6 +7,7 @@ environment variables:
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 
 env = os.environ
@@ -75,6 +76,8 @@ DJANGO_APPS = [
 # 써드파티 라이브러리
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",  # token blacklist
 ]
 
 # 프로젝트에서 생성한 앱
@@ -148,3 +151,35 @@ TIME_ZONE = "Asia/Seoul"
 USE_I18N = True
 
 USE_TZ = True
+
+# ------------------------------------------------
+# Thrid Party
+# ------------------------------------------------
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
+# DRF simplejwt 설정
+# https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=12),  # access token lifetime
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),  # refresh token lifetime
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": None,
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "USER_ID_FIELD": "email",  # user PK
+    "USER_ID_CLAIM": "user_id",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "JTI_CLAIM": "jti",
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(hours=1),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+}
--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("user/", include("apps.user.urls")),
 ]
 
 if settings.DEBUG:
