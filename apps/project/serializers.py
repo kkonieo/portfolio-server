3fc91878cec563138e501f0b02fdb8c52b20df9f
hmkim199 <hmkim199@gmail.com>
2022-01-28 17:44:19 +0900

    Implement retrieve projects (#33)
    * feat: delete useless serializer and view, working on retrieve projects
    
    * refactor: project view
    
    * feat: project pagenation
    
    * feat: project pagination and strip html tags, newlines
    
    * feat: show all user's projects when project query param not contain slug

--- apps/project/serializers.py
+++ apps/project/serializers.py
@@ -1,6 +1,20 @@
+from django.utils.html import strip_tags
+from django.utils.text import normalize_newlines
 from rest_framework import serializers
 
+from apps.user.models import User
+
 from .models import Project
+
+
+class LikerSerializer(serializers.ModelSerializer):
+    """
+    좋아요 누른 사람 slug, name
+    """
+
+    class Meta:
+        model = User
+        fields = ("slug", "name")
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -9,25 +23,25 @@ class ProjectSerializer(serializers.ModelSerializer):
     """
 
     thumbnail = serializers.ImageField(source="thumbnail.source")
+    likers = LikerSerializer(source="liker", many=True)
 
     class Meta:
         model = Project
-        fields = "__all__"
-
-
-class ProjectCardSerializer(ProjectSerializer):
-    """
-    프로젝트의 제목, 썸네일
-    """
-
-    class Meta(ProjectSerializer.Meta):
-        fields = ("title", "thumbnail")
+        fields = ("title", "content", "thumbnail", "likers")
 
 
 class ProjectSummarySerializer(ProjectSerializer):
     """
     프로젝트의 제목, 썸네일, 내용(미리보기 내용)
     """
+
+    content = serializers.SerializerMethodField()
+
+    def get_content(self, obj):
+        strip_string = strip_tags(obj.content).strip()
+        strip_string = normalize_newlines(strip_string)
+        strip_string = strip_string.replace("\n", " ")
+        return strip_string[:100]
 
     class Meta(ProjectSerializer.Meta):
         fields = ("title", "thumbnail", "content")
--- apps/project/views.py
+++ apps/project/views.py
@@ -1,37 +1,68 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Project
-from .serializers import (
-    ProjectCardSerializer,
-    ProjectSerializer,
-    ProjectSummarySerializer,
-)
+from .serializers import ProjectSerializer, ProjectSummarySerializer
 
 
-class BaseProjectList(APIView):
+class BaseProjectsView(APIView):
     """
     Base Project List class
     """
 
     serializer = ProjectSerializer
+    count = 10
+    page = 1
 
-    def get_user(self):
-        return self.request.user
+    def set_to_show_summary(self, query):
+        short = query.get("short")
+        if short:
+            short = short.lower()
+
+        if short == "true":
+            self.serializer = ProjectSummarySerializer
+
+    def set_pagination(self, count, page):
+        if count:
+            self.count = count
+        if page:
+            self.page = page
+
+    def interprete_query(self, query):
+        self.user_slug = query.get("slug")
+        self.set_to_show_summary(query)
+        self.set_pagination(query.get("count"), query.get("page"))
 
     def get(self, request):
-        # TODO: fix temp user to real user
-        user = "hmkim199@gmail.com"
-        # user = self.get_user()
-        projects = Project.objects.filter(author=user).order_by("-created_at")
+        """
+        query parameter (option): slug, page, count, short
+        slug: slug 전달 시 특정 유저의 프로젝트 반환. 아닐 시 전체 유저 프로젝트 반환. (최신 순)
+        page, count: 페이지네이션 원할 시 원하는 페이지와 갯수
+        short: true일 경우 프로젝트 요약 정보 반환. 아닐 시 상세 정보 반환.
+        """
+        self.interprete_query(request.GET)
+
+        # 전체 유저 프로젝트 목록. 최신 순
+        if not self.user_slug:
+            project_list = Project.objects.order_by("-created_at")
+
+        # 특정 유저 프로젝트 목록 필터링. 최신 순
+        else:
+            project_list = Project.objects.filter(author__slug=self.user_slug).order_by(
+                "-created_at"
+            )
+        paginator = Paginator(project_list, self.count)
+        projects = paginator.get_page(self.page)
+
         serializer = self.serializer(projects, many=True)
 
         return Response(serializer.data)
 
 
-class ProjectList(BaseProjectList):
+class ProjectsView(BaseProjectsView):
     def post(self, request):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
@@ -42,17 +73,7 @@ class ProjectList(BaseProjectList):
     # TODO: put, delete 함수도 작성
 
 
-class ProjectCardList(BaseProjectList):
-    """
-    Project 카드 리스트
-
-    프로젝트의 제목, 썸네일 리스트 최신순으로 반환합니다.
-    """
-
-    serializer = ProjectCardSerializer
-
-
-class ProjectSummaryList(BaseProjectList):
+class ProjectsSummaryView(BaseProjectsView):
     """
     Project 요약 리스트
 
