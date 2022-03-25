7870675ad19da5b03ac1ccf926f8944cf24cca1a
hmkim199 <hmkim199@gmail.com>
2022-03-25 20:39:54 +0900

    Fix models (#38)
    * fix: modify models via erd
    
    * fix: models via modified erd
    
    * fix: modified model migration
    
    Co-authored-by: hyeminKim <hmkim199@naver.com>

--- apps/core/models.py
+++ apps/core/models.py
@@ -21,6 +21,18 @@ class TimeStampModel(models.Model):
         abstract = True
 
 
+class DurationModel(models.Model):
+    """
+    시작일, 종료일 필드를 포함하는 추상 모델
+    """
+
+    started_at = models.DateTimeField(verbose_name="시작일", blank=True, null=True)
+    ended_at = models.DateTimeField(verbose_name="종료일", blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
 class Image(TimeStampModel):
 
     source = models.ImageField(verbose_name="이미지", upload_to=get_uuid_path)
--- apps/user/models.py
+++ apps/user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserM
 from django.db import models
 from pyexpat import model
 
-from apps.core.models import Image, TimeStampModel
+from apps.core.models import DurationModel, Image, TimeStampModel
 from apps.core.utility import generate_random_string
 from apps.tag.models import Position, Tech
 from apps.user.validators import NameValidator
@@ -79,6 +79,16 @@ class User(AbstractBaseUser, TimeStampModel, PermissionsMixin):
         blank=True,
         null=True,
     )
+    expected_salary = models.IntegerField(
+        verbose_name="희망 연봉",
+        blank=True,
+        null=True,
+    )
+    hobby = models.TextField(
+        verbose_name="취미",
+        blank=True,
+        null=True,
+    )
     positions = models.ManyToManyField(
         Position,
         verbose_name="포지션",
@@ -105,13 +115,20 @@ class User(AbstractBaseUser, TimeStampModel, PermissionsMixin):
 
 
 class Link(TimeStampModel):
+    """
+    사용자 관련 링크 (블로그, 깃허브 등)
+    """
+
     user = models.ForeignKey(
         User,
         verbose_name="사용자",
         on_delete=models.CASCADE,
         related_name="link_user",
     )
-    source = models.URLField(verbose_name="링크 URL", max_length=120)
+    source = models.URLField(
+        verbose_name="링크 URL",
+        max_length=120,
+    )
 
     class Meta:
         verbose_name_plural = "링크"
@@ -119,3 +136,122 @@ class Link(TimeStampModel):
 
     def __str__(self) -> str:
         return self.source
+
+
+class Education(DurationModel, TimeStampModel):
+    """
+    학력
+    """
+
+    user = models.ForeignKey(
+        User,
+        verbose_name="사용자",
+        on_delete=models.CASCADE,
+        related_name="education_user",
+    )
+    school = models.CharField(
+        verbose_name="학교명",
+        max_length=50,
+        blank=True,
+        null=True,
+    )
+    status = models.CharField(
+        verbose_name="재학 또는 졸업 상태",
+        max_length=10,
+        default="졸업",
+    )
+    department = models.CharField(
+        verbose_name="학과",
+        max_length=50,
+        default="컴퓨터공학과",
+    )
+    gpa = models.FloatField(
+        verbose_name="학점",
+        default=3.5,
+    )
+
+
+class Functions(TimeStampModel):
+    """
+    개발 기능
+    """
+
+    user = models.ForeignKey(
+        User,
+        verbose_name="사용자",
+        on_delete=models.CASCADE,
+        related_name="function_user",
+    )
+    name = models.CharField(
+        verbose_name="기능명",
+        max_length=50,
+        blank=True,
+        null=True,
+    )
+    description = models.TextField(
+        verbose_name="기능 설명",
+        blank=True,
+        null=True,
+    )
+
+
+class OtherExperience(DurationModel, TimeStampModel):
+    """
+    교육 및 기타 경험
+    """
+
+    user = models.ForeignKey(
+        User,
+        verbose_name="사용자",
+        on_delete=models.CASCADE,
+        related_name="other_experience_user",
+    )
+    title = models.CharField(
+        verbose_name="경험명",
+        max_length=50,
+        blank=True,
+        null=True,
+    )
+    achievement = models.TextField(
+        verbose_name="이수 교과 또는 주요 성취 등",
+        blank=True,
+        null=True,
+    )
+    tech = models.ManyToManyField(
+        Tech,
+        verbose_name="경험에서 사용한 기술 목록",
+        related_name="other_experiences",
+        blank=True,
+        null=True,
+    )
+
+
+class Career(DurationModel, TimeStampModel):
+    """
+    경력
+    """
+
+    user = models.ForeignKey(
+        User,
+        verbose_name="사용자",
+        on_delete=models.CASCADE,
+        related_name="career_user",
+    )
+    company = models.CharField(
+        verbose_name="회사명",
+        max_length=50,
+        blank=True,
+        null=True,
+    )
+    positions = models.ManyToManyField(
+        Position,
+        verbose_name="포지션",
+        related_name="careers",
+        blank=True,
+    )
+    tech = models.ManyToManyField(
+        Tech,
+        verbose_name="기술 목록",
+        related_name="careers",
+        blank=True,
+    )
