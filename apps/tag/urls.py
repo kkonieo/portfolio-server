8caf73fe9e35ffa7ca66173e03509c4006dd2da4
hmkim199 <hmkim199@gmail.com>
2022-04-06 21:01:23 +0900

    feat: implement retrieve position api by querystring

--- apps/tag/urls.py
+++ apps/tag/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.tag.views import TechView
+from apps.tag.views import PositionView, TechView
 
 urlpatterns = [
     path("tech", TechView.as_view()),
+    path("position", PositionView.as_view()),
 ]
--- apps/tag/views.py
+++ apps/tag/views.py
@@ -14,6 +14,10 @@ class TechView(APIView):
     """
 
     def get(self, request):
+        """
+        쿼리 파라미터로 keyword 넘기면 키워드 포함하는 모든 기술 스택 리턴,
+        쿼리 파라미터 넘기지 않으면 모든 기술 스택 리턴.
+        """
         keyword = request.GET.get("keyword")
 
         if not keyword:
@@ -25,4 +29,28 @@ class TechView(APIView):
                     {"message": "wrong input"}, status=status.HTTP_400_BAD_REQUEST
                 )
         serializer = TechSerializer(tech, many=True)
+        return Response(serializer.data)
+
+
+class PositionView(APIView):
+    """
+    포지션 API
+    """
+
+    def get(self, request):
+        """
+        쿼리 파라미터로 keyword 넘기면 키워드 포함하는 모든 포지션 리턴,
+        쿼리 파라미터 넘기지 않으면 모든 포지션 리턴.
+        """
+        keyword = request.GET.get("keyword")
+
+        if not keyword:
+            position = Position.objects.all()
+        else:
+            position = Position.objects.filter(name__icontains=keyword)
+            if not position:
+                return Response(
+                    {"message": "wrong input"}, status=status.HTTP_400_BAD_REQUEST
+                )
+        serializer = PositionSerializer(position, many=True)
         return Response(serializer.data)
