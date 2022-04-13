a7533d2d5243c22a23bf43aba2855e7e201271e6
hmkim199 <hmkim199@gmail.com>
2022-04-13 21:03:31 +0900

    fix: modify project view, serializer

--- apps/project/urls.py
+++ apps/project/urls.py
@@ -6,9 +6,7 @@ from apps.project.views import ProjectsView, ProjectView
 urlpatterns = [
     path("/<int:project_id>/likes", ProjectView.as_view()),
     path("/<int:project_id>", ProjectView.as_view()),
-    path("", ProjectsView.as_view()),  # localhost/projects
+    path("", ProjectsView.as_view()),
     path("/<int:project_id>/comments", CommentListView.as_view()),
     path("/<int:project_id>/comments/<int:comment_id>", CommentView.as_view()),
 ]
-# RESTFUL API
-# localhost/projects
